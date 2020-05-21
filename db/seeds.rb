2dfcc4b734f8416d367ca424ab4a282991968a66
rokylebrown <r.kyle.brown@gmail.com>
2020-05-20 22:15:09 -0700

    test herkou load time 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,41 +17,41 @@ m1 = Movie.create(
     imageUrl: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/godfather.png",
     feature: true,
     top_feature: true,
-    video: "TheGodfather.mp4"
+    video: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/TheGodfather.mp4"
 )   
 m2 = Movie.create(
     title: "Pulp Fiction",
     description: "Starring John Travolta, Samuel L. Jackson, Bruce Willis, Tim Roth, Ving Rhames, and Uma Thurman, it tells several stories of criminal Los Angeles.",
-    imageUrl: "pulp-fiction.png",
+    imageUrl: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/pulp-fiction.png",
     feature: true,
     top_feature: false,
-    video: "PulpFiction.mp4"
+    video: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/PulpFiction.mp4"
 )   
 m3 = Movie.create(
     title: "Ex Machina",
     description: "Ex Machina is a 2014 science fiction film written and directed by Alex Garland. Domhnall Gleeson, Alicia Vikander, and Oscar Isaac star in a story that follows a programmer who is invited by his CEO to administer the Turing test to an intelligent humanoid robot.",
-    imageUrl: "ExMachina.png",
+    imageUrl: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/ExMachina.png",
     feature: true,
     top_feature: false,
-    video: "ExMachina.mp4"
+    video: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/ExMachina.mp4"
 )   
 
 
 m4 = Movie.create(
     title: "Blue Velvet",
     description: "Blue Velvet is a 1986 American neo-noir mystery film written and directed by David Lynch. Blending psychological horror with film noir, the film stars Kyle MacLachlan, Isabella Rossellini, Dennis Hopper, and Laura Dern, and is named after Bobby Vinton's 1963 song of the same name.",
-    imageUrl: "BlueVelvet.png",
+    imageUrl: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/BlueVelvet.png",
     feature: true,
     top_feature: false,
-    video: "BlueVelvet.mp4"
+    video: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/BlueVelvet.mp4"
 )
 
 
 m5 = Movie.create(
     title: "Mad Max",
     description: "Mad Max: Fury Road is a 2015 post-apocalyptic action film co-written, produced, and directed by George Miller. Miller collaborated with Brendan McCarthy and Nico Lathouris on the screenplay . . .",
-    imageUrl: "mad_max.png",
+    imageUrl: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/mad_max.png",
     feature: true,
     top_feature: false,
-    video: "MadMax.mp4"
+    video: "https://aqua-app-dev.s3-us-west-1.amazonaws.com/MadMax.mp4"
 ) 
