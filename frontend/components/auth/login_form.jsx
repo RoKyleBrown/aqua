492461f024486cb081a44a9ca045ecef064cb7b1
rokylebrown <r.kyle.brown@gmail.com>
2020-12-07 15:03:21 -0800

    login

--- frontend/components/auth/login_form.jsx
+++ frontend/components/auth/login_form.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 
-class LoginForm extends React.Component {
+class LoginForm extends React.Component { 
     constructor(props) {
         super(props);
         this.state = {
